ff2a0bda6c21425dc8cb8a91177dc1136a71836f
Hajime UCHIMURA <uchimura@polyphony.co.jp>
2018-01-27 00:29:06 +0900

    OTF now returns 0-1, 1=100nits

--- include/colorsystem.hpp
+++ include/colorsystem.hpp
@@ -699,7 +699,7 @@ class OTF
         const float pq_c1 = 0.8359375f;       // 3424.0 / 4096.0 or pq_c3 - pq_c2 + 1.0;
         const float pq_c2 = 18.8515625f;      // ( 2413.0 / 4096.0 ) * 32.0;
         const float pq_c3 = 18.6875f;         // ( 2392.0 / 4096.0 ) * 32.0;
-        const float pq_C  = 10000.0f;
+        const float pq_C  = 100.0f;
 
         // Note that this does NOT handle any of the signal range
         // considerations from 2084 - this assumes full range (0 - 1)
@@ -709,44 +709,44 @@ class OTF
             L = 0.0;
         L = L / (pq_c2 - pq_c3 * Np);
         L = powf(L, 1.0f / pq_m1);
-        return L * pq_C; // returns cd/m^2
+        return L * pq_C; // returns 0-100, 1=100cd/m^2
     }
-    static const float Y_to_ST2084(const float &nit) // nit should be 0-10000(cd/m^2)
+    static const float Y_to_ST2084(const float &C) // C should be 0-100, 1=100cd/m^2
     {
-        if (nit <= 0.f)
+        if (C <= 0.f)
             return 0.f;
+        if (C>=100.f)
+            return 1.f;
         const float pq_m1 = 0.1593017578125f; // ( 2610.0 / 4096.0 ) / 4.0;
         const float pq_m2 = 78.84375f;        // ( 2523.0 / 4096.0 ) * 128.0;
         const float pq_c1 = 0.8359375f;       // 3424.0 / 4096.0 or pq_c3 - pq_c2 + 1.0;
         const float pq_c2 = 18.8515625f;      // ( 2413.0 / 4096.0 ) * 32.0;
         const float pq_c3 = 18.6875f;         // ( 2392.0 / 4096.0 ) * 32.0;
-        const float pq_C  = 10000.0f;
+        const float pq_C  = 100.0f;
 
         // Note that this does NOT handle any of the signal range
         // considerations from 2084 - this returns full range (0 - 1)
-        float L  = nit / pq_C;
+        float L  = C / pq_C;
         float Lm = powf(L, pq_m1);
         float N  = (pq_c1 + pq_c2 * Lm) / (1.0f + pq_c3 * Lm);
         N        = powf(N, pq_m2);
         return N;
     }
-    static const float Y_to_sRGB(const float &nits) // returns signal, 0-1, input nits [0-100]
-    {
-        const float C = nits / 100.f;
+    static const float Y_to_sRGB(const float &C) // returns signal, 0-1, input 0-1
+    {        
         return (C < 0.f) ? 0.f : (C < 0.0031308f) ? C * 12.92f : (1.055f * powf(C, 1.0f / 2.4f) - 0.055f);
     }
-    static const float sRGB_to_Y(const float &C) // returns nits, 0-100[cd/m^2]
+    static const float sRGB_to_Y(const float &C) // returns 0-1, 1=100 nits
     {
-        return (C < 0.04045f) ? C / 12.92f : powf((C + 0.055f) / 1.055f, 2.4f) * 100.f;
+        return (C < 0.04045f) ? C / 12.92f : powf((C + 0.055f) / 1.055f, 2.4f);
     }
-    static const float Y_to_BT709(const float &nits) // returns signal, 0-1, input nits [0-100]
+    static const float Y_to_BT709(const float &C) // returns signal, 0-1, input 0-1
     {
-        const float C = nits / 100.f;
         return (C < 0.f) ? 0.f : (C < 0.018f) ? C * 4.50f : (1.099f * powf(C, 0.45f) - 0.099f);
     }
     static const float BT709_to_Y(const float &C) // returns nits, 0-100[cd/m^2]
     {
-        return (C < 0.081f) ? C / 4.50f : powf((C + 0.099f) / 1.099f, 1.f / 0.45f) * 100.f;
+        return (C < 0.081f) ? C / 4.50f : powf((C + 0.099f) / 1.099f, 1.f / 0.45f);
     }
 
     static const float CV_to_IRE_SLog2(const float &cv)
@@ -761,17 +761,16 @@ class OTF
         const float WV    = 876.f / 1024.f; // 940-64
         return (ire * WV) + BLACK;
     }
-    static const float Y_to_SLog2(const float &nits) // returns signal, 0-1, input nits [0-100]
+    static const float Y_to_SLog2(const float &x) // returns signal, 0-1, input 0-1
     {
-        const float x = nits / 100.f;
         const float y = (x < 0.f) ? x * 3.53881278538813f + 0.030001222851889303f : (0.432699f * log10f(155.0f * x / 219.0f + 0.037584f) + 0.616596f) + 0.03f;
         return IRE_to_CV_SLog2(y);
     }
-    static const float SLog2_to_Y(const float &C) // returns nits, 0-100[cd/m^2]
+    static const float SLog2_to_Y(const float &C) // returns 0-1, 1=100cd/m^2
     {
         const float x = CV_to_IRE_SLog2(C);
         const float y = (x >= 0.030001222851889303f) ? 219.0f * (powf(10.0f, ((x - 0.616596f - 0.03f) / 0.432699f)) - 0.037584f) / 155.0f : (x - 0.030001222851889303f) / 3.53881278538813f;
-        return (y > 0.f) ? y * 100.f : 0.f;
+        return (y > 0.f) ? y : 0.f;
     }
     static const Tristimulus toScreen(TYPE type, const Tristimulus &scene, const float g = 1.f)
     {
